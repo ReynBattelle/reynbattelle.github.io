93b8f57904bc3cd0ab157befe77c3e42f104c5da
devonk15 <94760159+devonk15@users.noreply.github.com>
2024-11-23 11:22:49 -0500

    update for fancy

--- js/index.js
+++ js/index.js
@@ -1,6 +1,52 @@
-const a = document.getElementById('a');
+const a = document.getElementById("a");
+const home = document.getElementById("home");
+const projects = document.getElementById("projects");
 
-a.addEventListener("click", function() {
+const currentFile = window.location.pathname.split("/").pop();
+
+a.addEventListener("mouseover", () => {
+    a.style.cursor  = "pointer";
+})
+
+home.addEventListener("mouseover", () => {
+    home.style.cursor = "pointer";
+})
+
+projects.addEventListener("mouseover", () => {
+    projects.style.cursor = "pointer";
+})
+
+a.addEventListener("click", () => {
     document.location = "../";
 }); 
 
+home.addEventListener("click", () => {
+    document.location = "../index";
+})
+
+projects.addEventListener("click", () => {
+    document.location = "../html/pastprojects";
+})
+
+
+if (currentFile == "index") {
+    home.style.textDecoration = "underline"
+} else if (currentFile == "html/pastprojects") {
+    home.style.textDecoration = "underline"
+}
+
+function updateVisiblity() {
+    if (window.innerWidth <= 500) {
+        home.style.visibility = "hidden";
+        projects.style.visibility = "hidden";
+        home.innerText = "";
+        projects.innerText = "";
+    } else {
+        home.style.visibility = "visible";
+        projects.style.visibility = "visible";
+        home.innerText = "Home";
+        projects.innerText = "Projects";
+    }    
+}
+
+window.addEventListener("resize", updateVisiblity)
